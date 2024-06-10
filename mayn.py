dc7ccebb4ca7e9d78b3aa3a866da041b9c38337d
BassOk11 <marsel.gafiev@gmail.com>
2024-06-10 09:22:02 +0300

    Add files via upload

---
+++ mayn.py
@@ -0,0 +1,20 @@
+a = "proizvolenaya stroka"
+print(len(a))
+a = 20
+b = 5
+summa = a + b
+dif = b - a
+print(summa)
+print(dif)
+first = 20
+second = 5
+third = first - second
+print(first+second+third//3)  #ез переменной mean
+print(20 + 5 + 15//3) #проверка  предыдущей операции
+mean = first + second + third // 3
+print(mean)
+first_string = "вторник"
+second_string ="понедельник,"
+print(second_string, first_string)
+f = (a*b)+(a*mean)
+print(f**3) #использовал предыдущие переменные и полученные значения
